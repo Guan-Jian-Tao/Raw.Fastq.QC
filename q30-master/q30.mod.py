bad606e854847f27583964700f97157f93177ad3
Jiantao Guan <30754020+Guan-Jian-Tao@users.noreply.github.com>
2017-09-07 10:08:59 +0800

    Add files via upload

--- q30-master/q30.mod.py
+++ q30-master/q30.mod.py
@@ -4,6 +4,11 @@ import os,sys
 import fastq
 import time
 
+def getContentOf(target_list, seq):
+  # add a 1 for each nucleotide in target_list
+  target_count = sum(1 for x in seq if x in target_list)
+  return float(target_count)
+  
 def qual_stat(qstr):
     q20 = 0
     q30 = 0
@@ -24,6 +29,7 @@ def stat(filename):
     min_len = 150
     max_len = 150
     read_num = 0
+    Count_GC = 0
     while True:
         read = reader.nextRead()
         if read == None:
@@ -37,6 +43,8 @@ def stat(filename):
         q20, q30 = qual_stat(read[3])
         q20_count += q20
         q30_count += q30
+        Count_GC += getContentOf(['G', 'C', 'g', 'c'], read[1])
+
 
     #print "total bases","\t",total_count
     #print "q20 bases","\t",q20_count
@@ -45,8 +53,8 @@ def stat(filename):
     #print "q30 percents","\t",100 * float(q30_count)/float(total_count)
     #print "Read Length Distributon","\t",min_len,  max_len
     #print "Mean Read Length","\t",float(total_count)/float(read_num)
-    print "Total bases\tQ20 bases\tQ30 bases\tQ20 percents\tq30 percents\tRead Length Distributon\tMean Read Length"
-    print total_count,"\t",q20_count,"\t",q30_count,"\t",100 * float(q20_count)/float(total_count),"\t",100 * float(q30_count)/float(total_count),"\t",min_len,"-",max_len,"\t",float(total_count)/float(read_num)
+    print "Total bases\tQ20 bases\tQ30 bases\tQ20 percents\tq30 percents\tGC Cntent\tRead Length Distributon\tMean Read Length"
+    print total_count,"\t",q20_count,"\t",q30_count,"\t",100 * float(q20_count)/float(total_count),"\t",100 * float(q30_count)/float(total_count),"\t",100 * float(Count_GC)/float(total_count),"\t",min_len,"-",max_len,"\t",float(total_count)/float(read_num)
 
 def main():
     if len(sys.argv) < 2:
